751d386469597a76ba976c37e2a94fbfca7a23f7
Thang Quyet Tran <thangquyet.tran@dxc.com>
2022-06-22 16:09:44 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
   backend "s3" {
        bucket = "ttran321-terraformstr"
        key    = "githubaction"
-       role_arn = "arn:aws:iam::786678469955:user/ttran321-iam"
+       role_arn = "arn:aws:iam::786678469955:role/ttran321-s3-role"
        region = "us-west-2"
    }
 #arn:aws:iam::786678469955:user/ttran321-iam
