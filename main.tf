b5007d9899eeb103c34e452ee9abdcf7829ee0ac
Thang Quyet Tran <thangquyet.tran@dxc.com>
2022-06-22 23:23:18 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
 terraform {
-
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      Version = "~>3.27"
+    }
+  }
   required_version = ">=0.14.9"
+  
 
   backend "s3" {
        bucket = "ttran321-terraformstr"
